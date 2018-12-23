8925348e34fadbe02786eb97dbae0f25d283e376
xiaoHuarong <songhongjian231@163.com>
2018-12-23 20:42:11 +0800

    Init Project:<SpringBatchExamples>

---
+++ src/test/java/com/flower/springbatch/SpringbatchexampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.flower.springbatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbatchexampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
